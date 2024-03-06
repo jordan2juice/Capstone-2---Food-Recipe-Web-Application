c9447923b0a1ea6884bf2bc02db45b45ef70a692
jordan toujouse <jtoujo1@gmail.com>
2024-03-05 20:39:10 -0600

    Created files for sass and images for my web app.

---
+++ js files/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Below is the code for the footer to update the year of the copyright.
+document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()));
