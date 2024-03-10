4d1ecff2fe939bde54f1abd5ace55ceb7c5b50b9
jordan toujouse <jtoujo1@gmail.com>
2024-03-10 14:58:58 -0500

    trying to fix the fetch and the background images on my pages.

---
+++ js files/fetch.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const BASE_URL = "https://my-proxy-server-rkxw.onrender.com";
+
+//this fetch grabs the backend data and wakes up the server to get the data from the backend if it's not already awake.
+fetch("https://my-proxy-server-rkxw.onrender.com")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- js files/footer.js
+++ js files/footer.js
@@ -5,14 +5,3 @@ document
   .appendChild(document.createTextNode(new Date().getFullYear()));
 
 // const BASE_URL = "https://my-proxy-server-rkxw.onrender.com";
-
-fetch("https://my-proxy-server-rkxw.onrender.com")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
